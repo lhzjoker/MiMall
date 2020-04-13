3d681a18d2bb1c829549f928edc7e3889de7af1d
lhzjoker <2313169248@qq.com>
2020-04-14 00:56:44 +0800

    三种前端跨域

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,24 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+// import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  /* 发送请求，用axios.get的方式 */
+  mounted(){
+    /* 一个供我们调用的接口 */
+    let url = '/api/activity/servicetime'
+    /* promise结构 */
+    // axios.get(url).then(()=>{
+      
+    // })
+    jsonp(url,()=>{
+      
+    })
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,16 @@
-module.exports = {}
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        /*代理*/
+        proxy:{
+            '/api':{
+                target: 'https://www.imooc.com',    //目标服务器 host
+                changeOrigin: true,     //默认false，是否需要改变原始主机头为目标URL
+                pathRewrite: {
+                    '/api':''     // 重写请求，比如我们源访问的是/api，那么请求会被解析为空
+                    },
+            }
+        }
+    }
+}
