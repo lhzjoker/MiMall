199c11e095cf3403bfb7dabfbe2c8432fac39a0c
lhzjoker <2313169248@qq.com>
2020-04-27 00:35:05 +0800

    NavHeader

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
         /*代理*/
         proxy:{
             '/api':{
-                target: 'https://www.imooc.com',    //目标服务器 host
+                target: 'http://mall-pre.springboot.cn',    //目标服务器 host
                 changeOrigin: true,     //默认false，是否需要改变原始主机头为目标URL
                 pathRewrite: {
                     '/api':''     // 重写请求，比如我们源访问的是/api，那么请求会被解析为空
