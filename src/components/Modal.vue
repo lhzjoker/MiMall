596cd2bf03681e724101e3d204aba654409d6317
lhzjoker <2313169248@qq.com>
2020-05-01 17:05:14 +0800

    modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+    <transition name="slide">
+        <div class="modal" v-show="showModal">
+            <div class="mask"></div>
+            <div class="modal-dialog">
+                <div class="modal-header">
+                    <span>{{title}}</span>
+                    <a href="javascript:;" class="icon-close" @click="$emit('cancel')"></a>
+                </div>
+                <div class="modal-body">
+                    <slot name="body">标题</slot>
+                </div>
+                <div class="modal-footer">
+                    <a href="javascript:;" class="btn" v-if="btnType==1" @click="$emit('submit')">{{sureText}}</a>
+                    <a href="javascript:;" class="btn" v-if="btnType==2" @click="$emit('cancel')">{{cancelText}}</a>
+                    <div class="btn-group" v-if="btnType==3">
+                        <a href="javascript:;" class="btn" @click="$emit('submit')">{{sureText}}</a>
+                        <a href="javascript:;" class="btn-default" @click="$emit('cancel')">{{cancelText}}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+<script>
+    export default {
+        name: 'modal',
+        props: {
+            //弹框尺寸大小,小中大，默认是form表单
+            modalType: {
+                type: String,
+                default: 'form',
+            },
+            //弹框标题
+            title: String,
+            //按钮类型,确定取消按钮
+            btnType: String,
+            sureText: {
+                type: String,
+                default: '确定'
+            },
+            cancelText: {
+                type: String,
+                default: '取消'
+            },
+            showModal: {
+                type: Boolean
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    @import './../assets/scss/mixin.scss';
+    @import './../assets/scss/config.scss';
+    @import './../assets/scss/modal.scss';
+</style>
