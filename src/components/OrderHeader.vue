2a2237885619532f43846673ecced134e695ee12
lhzjoker <2313169248@qq.com>
2020-05-08 20:30:26 +0800

    order-header

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,11 +1,81 @@
 <template>
-    <div>
-        order-header
+  <div class="order">
+    <div class="container clearfix">
+      <div class="logo fl">
+        <a href="/#/index"></a>
+      </div>
+      <div class="title">
+        <h2 class="fl">{{title}}<slot name="tips"></slot></h2>
+      </div>
+      <div class="username fr">
+        <a href="javascript:;">{{this.$store.state.username}}</a>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default{
-        name: 'order-header',
-    }
+export default {
+  name: "order-header",
+  props:{
+      title:String
+  }
+};
 </script>
+<style lang="scss">
+@import "./../assets/scss/mixin.scss";
+@import "./../assets/scss/config.scss";
+.order {
+  padding: 30px 0;
+  border-bottom: 2px solid $colorA;
+  .logo {
+    width: 55px;
+    height: 55px;
+    display: inline-block;
+    background-color: $colorA;
+    a {
+      display: inline-block;
+      width: 110px;
+      height: 55px;
+      &:before {
+        content: "";
+        @include bgImg(55px, 55px, "/imgs/mi-logo.png");
+        display: inline-block;
+        transition: all 0.3s;
+      }
+      &:after {
+        content: "";
+        @include bgImg(55px, 55px, "/imgs/mi-home.png");
+        display: inline-block;
+        transition: all 0.3s;
+      }
+      &:hover:before {
+        margin-left: -55px;
+        transition: all 0.3s;
+      }
+    }
+  }
+  .title {
+    h2 {
+      font-size: 28px;
+      color: #333333;
+      margin-left: 55px;
+      line-height: 55px;
+    }
+    span{
+      font-size: 14px;
+      margin-left: 17px;
+      color: #999999;
+      line-height: 55px;
+    }
+  }
+  .username {
+    a {
+      font-size: 16px;
+      color: #666666;
+      display: inline-block;
+    }
+    line-height: 55px;
+  }
+}
+</style>
--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <order-header></order-header>
+        <order-header title="我的购物车">
+            <template v-slot:tips>
+                <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算</span>
+            </template>
+        </order-header>
         <router-view></router-view>
-        cart
+        <service-bar></service-bar>
         <nav-footer></nav-footer>
     </div>
 </template>
@@ -10,11 +14,13 @@
 <script>
     import OrderHeader from './../components/OrderHeader'
     import NavFooter from './../components/NavFooter'
+    import ServiceBar from './../components/ServiceBar'
     export default{
         name: 'cart',
         components: {
             OrderHeader,
-            NavFooter
+            NavFooter,
+            ServiceBar
         }
     }
 </script>
