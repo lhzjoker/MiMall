1f13d566be5b5e67d7c9a96b3b1dcbcb62a77854
lhzjoker <2313169248@qq.com>
2020-05-15 00:47:22 +0800

    mapState

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -5,77 +5,92 @@
         <a href="/#/index"></a>
       </div>
       <div class="title">
-        <h2 class="fl">{{title}}<slot name="tips"></slot></h2>
+        <h2 class="fl">{{title}}<slot name="tips"></slot>
+        </h2>
       </div>
       <div class="username fr">
-        <a href="javascript:;">{{this.$store.state.username}}</a>
+        <a href="javascript:;">{{username}}</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "order-header",
-  props:{
-      title:String
-  }
-};
+  import { mapState } from 'vuex';
+  export default {
+    name: "order-header",
+    props: {
+      title: String
+    },
+    computed: {
+      ...mapState(['username'])
+    },
+  };
 </script>
 <style lang="scss">
-@import "./../assets/scss/mixin.scss";
-@import "./../assets/scss/config.scss";
-.order {
-  padding: 30px 0;
-  border-bottom: 2px solid $colorA;
-  .logo {
-    width: 55px;
-    height: 55px;
-    display: inline-block;
-    background-color: $colorA;
-    a {
-      display: inline-block;
-      width: 110px;
+  @import "./../assets/scss/mixin.scss";
+  @import "./../assets/scss/config.scss";
+
+  .order {
+    padding: 30px 0;
+    border-bottom: 2px solid $colorA;
+
+    .logo {
+      width: 55px;
       height: 55px;
-      &:before {
-        content: "";
-        @include bgImg(55px, 55px, "/imgs/mi-logo.png");
-        display: inline-block;
-        transition: all 0.3s;
-      }
-      &:after {
-        content: "";
-        @include bgImg(55px, 55px, "/imgs/mi-home.png");
-        display: inline-block;
-        transition: all 0.3s;
-      }
-      &:hover:before {
-        margin-left: -55px;
-        transition: all 0.3s;
-      }
-    }
-  }
-  .title {
-    h2 {
-      font-size: 28px;
-      color: #333333;
-      margin-left: 55px;
-      line-height: 55px;
-    }
-    span{
-      font-size: 14px;
-      margin-left: 17px;
-      color: #999999;
-      line-height: 55px;
-    }
-  }
-  .username {
-    a {
-      font-size: 16px;
-      color: #666666;
       display: inline-block;
+      background-color: $colorA;
+
+      a {
+        display: inline-block;
+        width: 110px;
+        height: 55px;
+
+        &:before {
+          content: "";
+          @include bgImg(55px, 55px, "/imgs/mi-logo.png");
+          display: inline-block;
+          transition: all 0.3s;
+        }
+
+        &:after {
+          content: "";
+          @include bgImg(55px, 55px, "/imgs/mi-home.png");
+          display: inline-block;
+          transition: all 0.3s;
+        }
+
+        &:hover:before {
+          margin-left: -55px;
+          transition: all 0.3s;
+        }
+      }
     }
-    line-height: 55px;
+
+    .title {
+      h2 {
+        font-size: 28px;
+        color: #333333;
+        margin-left: 55px;
+        line-height: 55px;
+      }
+
+      span {
+        font-size: 14px;
+        margin-left: 17px;
+        color: #999999;
+        line-height: 55px;
+        font-weight: 200;
+      }
+    }
+
+    .username {
+      a {
+        font-size: 16px;
+        color: #666666;
+        display: inline-block;
+      }
+      line-height: 55px;
+    }
   }
-}
 </style>
