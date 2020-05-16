5bf96f5b6a1d48554c87d97552c6c644e8f895e4
lhzjoker <2313169248@qq.com>
2020-05-17 01:04:09 +0800

    支付宝支付页面

--- src/pages/alipay.vue
+++ src/pages/alipay.vue
@@ -1,11 +1,48 @@
 <template>
-    <div>
-        alipay
+    <div class="ali-pay">
+        <order-header title="订单支付">
+            <template v-slot:tips>
+                <span>请谨防钓鱼链接或诈骗电话，了解更多</span>
+            </template>
+        </order-header>
+        <loading v-if="loading"></loading>
+        <div class="form" v-html="content"></div>
     </div>
 </template>
 
 <script>
-    export default{
+    import OrderHeader from './../components/OrderHeader';
+    import Loading from './../components/Loading';
+    export default {
         name: 'alipay',
+        data() {
+            return {
+                orderId: this.$route.query.orderId,      //订单号
+                content: '',     //支付宝页面html源码
+                loading: true   //是否展示loading
+            }
+        },
+        components: {
+            OrderHeader,
+            Loading
+        },
+        mounted() {
+            this.submitPay()
+        },
+        methods: {
+            submitPay() {
+                this.axios.post('/pay', {
+                    orderId: this.orderId,
+                    orderName: 'Vue小米高仿商城',
+                    amount: 0.01,
+                    payType: 1,
+                }).then((res) => {
+                    this.content = res.content
+                    setTimeout(()=>{
+                        document.forms[0].submit();     //提交第一个表单数据，跳转到支付宝页面
+                    },100)
+                })
+            }
+        },
     }
 </script>
