88c89e6e49461df40f8f27ded9ec8120588ee73b
lhzjoker <2313169248@qq.com>
2020-05-18 00:35:27 +0800

    http状态码错误拦截

--- src/main.js
+++ src/main.js
@@ -17,19 +17,23 @@ axios.defaults.baseURL = '/api'
 axios.defaults.timeout = 8000
 
 //接口错误拦截
-axios.interceptors.response.use(function(response){
-  let res = response.data
+axios.interceptors.response.use(function(response){   //拦截页面返回数据状态码
+  let res = response.data   //返回的数据为response.data，包括status和data
   if(res.status == 0){      //状态码为0代表登陆成功
     return res.data
-  }else if(response.status == 10){    //状态码为10代表未登陆，跳转到登陆页面
-    if(location.hash!='#/index'){     //如果没有登录则只能访问首页
-      window.location.href = '/#/login'   //跳转到登陆页面
+  }else if(res.status == 10){    //状态码为10代表未登陆，跳转到登陆页面
+    if(location.hash!="#/index"){     //如果没有登录则只能访问首页
+      window.location.href = "/#/login"   //跳转到登陆页面
     }
     return Promise.reject()   //发生错误，promise返回拒绝值
   }else{
     Message.error(res.msg)
     return Promise.reject()   //发生错误，promise返回拒绝值
   }
+},(error)=>{    //拦截http请求状态码（请求发送失败）
+  let res = error.response
+  Message.error(res.data.message)
+  return Promise.reject()   //发生错误，promise返回拒绝值
 })
 
 //mock开关，需要使用mock的时候使用
